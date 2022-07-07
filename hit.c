340829026e5a9a800704be325b5d971cf97d1ece
kazma_linux <pkzmnz7zo@gmail.com>
2022-07-07 20:34:36 +0900

    add hitbox

--- hit.c
+++ hit.c
@@ -3,6 +3,26 @@
 #include "hit.h"
 #include "objects.h"
 
+GLdouble static const cHitBoxCarrot = 1.0;
+GLdouble static const cHitBoxPoisonCarrot = 1.0;
+GLdouble static const cHitBoxDiamond = 1.0;
+
+GLdouble getObjectHitBox(int index){
+	switch (getObjectType(index))
+	{
+	case CARROT:
+		return cHitBoxCarrot;
+		break;
+	case P_CARROT:
+		return cHitBoxPoisonCarrot;
+	case DIAMOND:
+		return cHitBoxDiamond;
+	default:
+		return 0.0;
+		break;
+	}
+}
+
 // let 0 is player
 // if hit with argument object, true
 GLboolean isHit(int index){
@@ -10,7 +30,7 @@ GLboolean isHit(int index){
 	GLdouble objectLocation = getObjectX(index);
 	GLdouble distance = fabs(playerLocation - objectLocation);
 	GLdouble playerHitBox = getObjectHitBox(ME);
-	GLdouble objectHitBox = getObjectHitBox();
+	GLdouble objectHitBox = getObjectHitBox(index);
 	GLdouble hitReach = playerHitBox + objectHitBox;
 	//dits < hitReach then hit
 	//  ---        ------- hit box
--- hit.h
+++ hit.h
@@ -1,5 +1,5 @@
 #ifndef HIT_H
 #define HIT_H
 #include <GL/glut.h>
-GLboolean isHit(int);
+GLboolean isHit(int index);
 #endif
